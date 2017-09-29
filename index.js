6ed179f543d85015615ab8739db36e3966195a68
smithwebtek <brad@smithwebtek.com>
2017-09-28 21:58:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = "joe"
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase()
+  return customerName
+}
+
+function setBestCustomer(asdf){
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob'
+}
+
+function reassignLeastFavorite(){
+  changeLeastFavoriteCustomer()
+} 
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "sam"
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = "tim"
+  let favoriteCustomer = "moe"
+}
