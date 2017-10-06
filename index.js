c104844beeddbd0bd992e2aff14b9c32f6a2a8a7
Mel Chang <mel510@users.noreply.github.com>
2017-10-06 13:24:06 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+customerName = 'bob';
+
+function upperCaseCustomerName () {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer () {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer () {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Name';
+
+function changeLeastFavoriteCustomer () {
+  leastFavoriteCustomer = 'Someone Else';
+}
+
+function attemptTwoFavoriteCustomers () {
+  let favoriteCustomer = 'Person1';
+  let favoriteCustomer = 'Person1';
+}
