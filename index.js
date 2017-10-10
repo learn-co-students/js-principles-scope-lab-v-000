7882e172ae97109b5aedef13a7bc6611a202a6dd
skmcloughlin <skmcloughlin@gmail.com>
2017-10-10 04:57:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+customerName = "bob"
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(input) {
+  bestCustomer = input
+}
+
+function changeLeastFavoriteCustomer() {
+  throw new TypeError
+}
+
+function attemptTwoFavoriteCustomers(input) {
+  throw new SyntaxError
+}
