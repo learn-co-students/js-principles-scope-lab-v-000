5fda8ef5f52f6d23c753e2c2e9357ce94c3225bc
Luisa Scavo <luisascavo@gmail.com>
2017-10-11 16:57:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'alice';
+
+upperCaseCustomerName = () => customerName = customerName.toUpperCase();
+setBestCustomer = () => bestCustomer = 'not bob';
+overwriteBestCustomer = (name) => bestCustomer = name;
+changeLeastFavoriteCustomer = () => leastFavoriteCustomer = 'so and so'
+attemptTwoFavoriteCustomers = () => {
+  let bestCustomer = 'alice'
+  let bestCustomer = 'bob'
+}
