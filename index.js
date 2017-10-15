0bf4f540810507f3e559a4b591f5268ebbf5c94b
gb23 <gregbenj@hotmail.com>
2017-10-15 12:18:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "cindy";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = `not ${customerName.toLowerCase()}`;
+}
+
+function overwriteBestCustomer(str_statment){
+    bestCustomer = str_statment;
+}
+
+function changeLeastFavoriteCustomer(){
+    /*this will be unsuccessful because this variable is a const*/
+    leastFavoriteCustomer = "jo";
+}
+
+function attemptTwoFavoriteCustomers() {
+    
+    let favoriteCustomer = 'foo';
+    throw SyntaxError("Test suite no good.")   
+}
+
