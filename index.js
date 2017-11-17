56d0af81e6990bda4cfaf7ae33b3bb74fdf84660
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-16 20:30:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'piter'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'poter';
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'jomney';
+  let favoriteCustomer = 'joby';
+}
+
+
+
