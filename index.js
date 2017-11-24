391ff9d5df60b1daade684cfff3dd49d3ffa8a5a
Brandon Hosley <brandon.hosley@csuglobal.edu>
2017-11-24 21:45:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+customerName = "bob";
+const leastFavoriteCustomer = customerName;
+let favoriteCustomer = customerName
+
+function upperCaseCustomerName(){
+  return customerName = customerName.toUpperCase();
+};
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+};
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+};
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'not bob';
+};
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = 'not bob'
+  const favoriteCustomer = 'still not bob'
+};
