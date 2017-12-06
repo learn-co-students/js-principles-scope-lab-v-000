c77f2b478f99b26077a7bd5477d014b9818e387c
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-06 20:12:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = "jeff"
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "barry"
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = "jerry"
+  let favoriteCustomer = "jung li"
+}
