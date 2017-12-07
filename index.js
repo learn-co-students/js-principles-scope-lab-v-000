76a286e1a858eddb4b19dfff8384012f24b15cc2
hyoyou <heatherhyou@gmail.com>
2017-12-07 12:27:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'definitely bob';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bob, for sure';
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'robert';
+  let favoriteCustomer = 'roberto';
+}
