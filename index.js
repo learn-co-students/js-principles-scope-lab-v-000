626dd696a61634e3711ca84c20b07938d5a5a74b
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-12-22 17:51:31 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer="Tasha"
+var bestCustomer
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+  return customerName
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+  return bestCustomer
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer="Braydon"
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = "braydon"
+  let favoriteCustomer = "BRAYDONNNNN"
+}
