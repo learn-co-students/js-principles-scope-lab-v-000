0a389f09b6e10e0ecd64b9992585b69ecee8c92e
dbfarms <matt.hunger@gmail.com>
2018-01-10 22:04:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+const leastFavoriteCustomer = "not bob";
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(newFavorite) {
+    bestCustomer = newFavorite
+}
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bob2"
+}
+
+
