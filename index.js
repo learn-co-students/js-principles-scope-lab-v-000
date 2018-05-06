90677ee4a1b31e79d50d73474def27327fb50935
dwieb <dwayne.harmon@gmail.com>
2018-05-05 23:44:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+function upperCaseCustomerName () {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer () {
+  bestCustomer = 'maybe bob'
+}
+
+
+const leastFavoriteCustomer = 'definitely donald trump';
+
+function changeLeastFavoriteCustomer () {
+  leastFavoriteCustomer = 'sour fred';
+}
