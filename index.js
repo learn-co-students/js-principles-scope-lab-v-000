d3d457c9b06aa59d26f05d17201ce64533d6c872
Emmanuel Owusu <e_owusu@live.com>
2018-05-09 05:43:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName;
+var customerName = "bob"
+https://learn.co/tracks/full-stack-web-development-v4/javascript/principles/scope-lab#
+function upperCaseCustomerName(){
+ customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer(){
+  bestCustomer = "not bob";
+}
+
+
+function overwriteBestCustomer(){
+   bestCustomer = "maybe bob";
+}
+
+
+const leastFavoriteCustomer = "Reni";
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "jeze";
+}
