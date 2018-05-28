8e43724ce5f8a5be6d091b9d32034c0e94ad60fb
Robert Hughes <roberts.ginger.email@gmail.com>
2018-05-28 14:10:55 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(sentence) {
+  bestCustomer = sentence
+}
+
+const leastFavoriteCustomer = 'not bob'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bob'
+}
