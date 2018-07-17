abe84d3b57911ea14c97a270f78ac247b6c1402b
AubreyNeimeier <neimeier.1@osu.edu>
2018-07-17 08:37:26 -0700

    first 3 tests passing

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = "bob";
+// customerName has global scope here
+
+function upperCaseCustomerName() {
+   return customerName = customerName.toUpperCase();
+} 
+
+// remember we have to explicitly RETURN the value we want
+
+
+function setBestCustomer() {
+    // bestCustomer is declared in global scope here
+   bestCustomer = "not bob";
+   return bestCustomer;
+}
