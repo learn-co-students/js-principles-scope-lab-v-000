cb66ff2703bdeed8b5487fbfdea1bf5ae1aa311a
AubreyNeimeier <neimeier.1@osu.edu>
2018-07-17 09:02:05 -0700

    completes notes and lab

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 var customerName = "bob";
 // customerName has global scope here
 
+const leastFavoriteCustomer = "henry"
+//constant in global scope. when we try to overwrite within a function we see a typeError
+
 function upperCaseCustomerName() {
    return customerName = customerName.toUpperCase();
 } 
@@ -13,4 +16,15 @@ function setBestCustomer() {
     // bestCustomer is declared in global scope here
    bestCustomer = "not bob";
    return bestCustomer;
+}
+
+function overwriteBestCustomer () {
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+    // because bestCustomer was declared in global scope, we are able 
+    // to overwrite the variable without any hiccups. This is dangerous! 
+}
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = "not henry";
 }
