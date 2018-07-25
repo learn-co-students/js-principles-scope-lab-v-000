94b7c8ab42492e9cf8ca8461a8ff99e69f0d5815
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-25 13:44:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  customerName = 'BOB'
+  return customerName
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+  return bestCustomer
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+  return bestCustomer
+}
+
+const leastFavoriteCustomer = "original content"
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'changed info'
+  return leastFavoriteCustomer
+}
