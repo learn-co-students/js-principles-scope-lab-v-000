c6e7abe36bcf4aa57e5eb1ad86017bad29d8fcf0
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-07-28 14:05:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+const leastFavoriteCustomer = 'joe'
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(arg) {
+  bestCustomer = arg;
+}
+
+function changeLeastFavoriteCustomer () {
+  leastFavoriteCustomer = 'john';
+}
