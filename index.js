c47618421e5822ef099903bf9c8a0c4db9db1f00
Pan Cheng <pchengeducation@hotmail.com>
2018-08-03 05:27:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+  return customerName;
+};
+function setBestCustomer(){
+  bestCustomer = "not bob";
+};
+function overwriteBestCustomer(newFavorite){
+  bestCustomer = newFavorite;
+};
+
+const leastFavoriteCustomer = "John";
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "me";
+};
