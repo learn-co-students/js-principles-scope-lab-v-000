5a5569367ee85dd20c9044d96d6f4513f9f4515d
Victoria Meng <nights1ststar@protonmail.com>
2018-08-14 03:52:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 var customerName = 'bob';
 function upperCaseCustomerName() {
-  customerName = 'BOB';
+  customerName = customerName.toUpperCase();
 }
 function setBestCustomer() {
   bestCustomer = 'not bob';
