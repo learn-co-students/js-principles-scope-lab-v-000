31f152c3f43f79ee5723baf6bd438934d64cc1f9
arye-dov-eidelman <aryedov.e@gmail.com>
2018-08-27 14:49:04 -0400

    upto last test

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+customerName = "bob";
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = `not ${customerName.toLowerCase()}`;
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = `maybe ${customerName.toLowerCase()}`;
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = customerName;
+  // throw TypeError;
+}
