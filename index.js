f39053986ccd0898d626202ad29b2c44f6aea15a
dalia <sawaya.dalia@gmail.com>
2018-09-01 14:28:11 +0000

    Done.

--- index.js
+++ index.js
@@ -2,39 +2,10 @@
 
 var customerName = 'bob';
 
-const leastFavoriteCustomer = 'sour fred';
+const leastFavoriteCustomer = 'sour fred'
 
 function upperCaseCustomerName () {
   return customerName = customerName.toUpperCase();
-}
-
-
-
-function upperCaseCustomerName () {
-  return customerName = customerName.toUpperCase();
-}
-
-
-
-function changeLeastFavoriteCustomer () {
-  leastFavoriteCustomer = 'sour fred';
-}
-
-function setBestCustomer () {
-  bestCustomer = 'not bob';
-}
-
-function overwriteBestCustomer (newFavorite) {
-  bestCustomer = newFavorite;
-}
-
-function congratulateCustomer () {
-  return `congrats ${favoriteCustomer}`;
-}
-
-function attemptTwoFavoriteCustomers () {
-  let favoriteCustomer = 'bar';
-  let favoriteCustomer = 'not bar';
 }
 
 function changeLeastFavoriteCustomer () {
