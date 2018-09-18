ff512eb60497dc63b4c8c740462ad4d88eff03fb
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-18 13:49:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = `not ${customerName.toLowerCase()}`;
+  return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = `maybe ${customerName.toLowerCase()}`;
+  return bestCustomer;
+}
+
+const leastFavoriteCustomer = "bob"
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "henry";
+}
