25c643c151cdb2fcf7dd7a6788b0f589f24dbcb6
Jayon Kim <jpkim921@gmail.com>
2018-09-27 04:26:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+  // console.log(customerName);
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  // var bestCustomer;
+  bestCustomer = 'not bob';
+  // return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'wall clock';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'digital clock';
+}
