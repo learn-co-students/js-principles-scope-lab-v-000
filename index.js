3ce77fa95a005e27f06dc77e4d5f465c05e68da4
jokerdelta <jokerdelta@yahoo.com>
2018-10-28 16:27:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+var leastFavoriteCustomer;
+
+function upperCaseCustomerName() {
+
+  customerName = customerName.toUpperCase()
+
+  return customerName;
+}
+
+function setBestCustomer(){
+
+  bestCustomer = 'not bob';
+
+  return bestCustomer;
+
+}
+
+function overwriteBestCustomer(){
+
+  bestCustomer = 'maybe bob';
+
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer.name;
+}
