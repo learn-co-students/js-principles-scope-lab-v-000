361fd22ef58ce1cb8b6c915d3234654e7a758bb0
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-02 09:56:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName
+
+function upperCaseCustomerName() {
+customer = customerName.toUpperCase();
+};
+
+function setBestCustomer() {
+var bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+var bestCustomer = "new favorite";
+}
+
+function leastFavoriteCustomer() {
+var leastFavoriteCustomer = "name";
+}
+
+
+function changeLeastFavoriteCustomer() {
+var changeLeastFavoriteCustomer = "someone else";
+}
