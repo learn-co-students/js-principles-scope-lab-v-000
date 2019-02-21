852e1d80aab0891c1b5b0076880e394faa6649bd
Mohammed Noori <kmanoorib@gmail.com>
2019-02-21 02:45:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+customerName = "bob";
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  return bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  return bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "someone";
+
+function changeLeastFavoriteCustomer() {
+  return leastFavoriteCustomer = "no one";
+}
