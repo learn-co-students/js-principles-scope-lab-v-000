45459617aa60add58f60308402efebba0fd698e5
Ludmilla Aires Negri <ludaires@gmail.com>
2019-03-09 15:02:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomes =  'Daniel';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+};
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+};
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+};
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomes = 'Lud'; 
+};
