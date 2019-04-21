f4d34ecb7021f44dfb708f1458c7ed1b5d110c2e
sjwakean <swakeman@ca.rr.com>
2019-04-20 18:32:41 -0700

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,22 @@
 var customerName = "bob";
 
 function upperCaseCustomerName() {
-    customerName.toUpperCase();
+    //redefine the variable HAVE TO SET customerName again!
+    customerName = customerName.toUpperCase();
 }
 
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+function leastFavoriteCustomer() {
+    const leastFavoriteCustomer = "initial value";
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "another value";
+}
