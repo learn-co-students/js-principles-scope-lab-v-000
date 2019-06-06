f9090df42e2d450a5ded79ba81ce185513e3c47b
Jonathan Stone <jpstone@protonmail.com>
2019-06-06 02:34:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'xyz';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(name) {
+  bestCustomer = name;
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'abc';
+}
