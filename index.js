e627f835964b72a81a18ed2d1edcdc739e460811
Erica Murdock-Waters <ericahpa@gmail.com>
2019-09-17 17:08:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+// Fix the Scope Lab
+
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Kent'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Kerry'
+}
